9e72574a11895f4c579d8680281958a5367ebeae
Manjula36 <manjulavdt@gmail.com>
2024-01-08 13:10:59 +0530

    Add project files.

---
+++ Jan5/Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jan5
+{
+    internal class Class2
+    {
+        // more features 
+        static void Main(string[] args)
+        {
+            Thread cuTh = Thread.CurrentThread;//property
+            cuTh.Name = "Hi Thread";
+            Console.WriteLine($"the name of thread    {cuTh.Name}");
+            Console.WriteLine($"the id of thread is {cuTh.ManagedThreadId}");
+            Console.WriteLine($"Priority of thread {cuTh.Priority}");
+            Console.WriteLine($"state of thread  {cuTh.ThreadState}");
+            Console.WriteLine($"thread is alive {cuTh.IsAlive}");
+
+        }
+    }
+}
---
+++ Jan5/Class4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jan5
+{
+    internal class Class4
+    {
+        static void Fun1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Function 1 Called {0}", i);
+                Thread.Sleep(1000);// 1 second
+            }
+        }
+        static void Main(string[] args)
+        {//parent thread 
+            Console.WriteLine("parent thread starts");
+            Thread childTh1 = new Thread(Fun1);
+            childTh1.IsBackground = true;// now child thread is back ground Thread
+            childTh1.Start();//BGT
+            Console.WriteLine("Parent thread stopped");
+        }
+    }
+}
+
+   
